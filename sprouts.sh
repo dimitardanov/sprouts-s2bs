2194adc6fad468d0b9081f7b1a214ee9992bd3e2
Dimitar Danov <dimitardanov@users.noreply.github.com>
2016-12-20 10:37:59 +0200

    Added npm install to the sprouts init script

--- sprouts.sh
+++ sprouts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install node packages
+npm install;
+
 # copy Bootstrap SCSS files
 cp -r node_modules/bootstrap-sass/assets/stylesheets/bootstrap/ src/scss/vendors/;
 cp node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss src/scss/vendors;
